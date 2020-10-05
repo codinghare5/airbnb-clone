2374a6ba28a8234e8f2aebfc3736da9769db88f4
Marcin Mukosiej <marcinmukosiej@gmail.com>
2020-10-05 20:11:34 +0100

    framing banner done, now framing cards section

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
 import './Banner.css'
+import Search from './Search';
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false);
+
     return (
         <div className="banner">
-            
+            <div className="banner__search">
+                {showSearch && <Search/>}
+                <Button
+                    onClick={() => setShowSearch(!showSearch)} 
+                    className="banner__searchButton" 
+                    variant="outlined" 
+                >{showSearch ? 'Hide' : 'Search Dates'}</Button>
+            </div>
+            <div className="banner__info">
+                <h1>Get out and stretch your imagination</h1>
+                <h5>
+                    Plan a different king of gatewat to uncoverthe hidden gems near you.
+                </h5>
+                <Button
+                    variant="outlined" >Explore Nearby</Button>
+            </div>
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Banner from './Banner';
 import './Home.css';
 
 function Home() {
     return (
         <div className="home" >
-            
+            <Banner />
+
+            <div className="home__section">
+
+            </div>
         </div>
     )
 }
